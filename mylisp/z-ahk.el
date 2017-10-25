bf535ab9261e4bb52c5e70667c8140b20bee4413
Dongjiu Zhu <donzhu@microsoft.com>
2017-10-25 18:22:08 +0800

    update bookmarks.org

--- mylisp/z-ahk.el
+++ mylisp/z-ahk.el
@@ -161,4 +161,4 @@
   (let ((title "UcmBusinessAnalyticsCube -"))
     (if (z-windows-title-contains title)
         (call-process "C:\\Users\\donzhu\\github\\tools4win\\activate_win.exe" nil 0 nil title)
-	  (z-run-in-cmd "\"C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\IDE\\devenv.exe\" D:\\work\\UCM-BusinessAnalytics\\private\\Cubes\\UcmBusinessAnalyticsCube\\UcmBusinessAnalyticsCube.sln" "Business Analytics" t))))
+	  (z-run-in-cmd "\"C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\IDE\\devenv.exe\" D:\\work\\UCM-BusinessAnalytics\\private\\UcmBusinessAnalyticsCube\\UcmBusinessAnalyticsCube.sln" "Business Analytics" t))))
