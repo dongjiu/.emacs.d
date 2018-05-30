a7313b43d733240bc011a03e0d2ce544f25e9696
Dongjiu Zhu <dongjiu.zhu@hotmail.com>
2018-05-30 07:52:02 +0000

    Adjust settings for centos

--- init.el
+++ init.el
@@ -1,4 +1,5 @@
 (require 'package)
+(require 'org)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/"))
 
@@ -12,13 +13,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(package-selected-packages
-   (quote
-    (zenburn-theme htmlize xahk-mode web-mode typescript-mode csharp-mode ahk-mode))))
+ '(ansi-color-faces-vector [default default default italic underline success warning error])
+ '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-safe-themes (quote ("a4df5d4a4c343b2712a8ed16bc1488807cd71b25e3108e648d4a26b02bc990b3" "9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" default)))
+ '(package-selected-packages (quote (zenburn-theme htmlize xahk-mode web-mode typescript-mode csharp-mode ahk-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
