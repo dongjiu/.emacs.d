497ed0d23e8542537de2c5b423b475bb8f6424bd
Dongjiu Zhu <dongjiu.zhu@hotmail.com>
2018-05-30 07:57:04 +0000

    set safe theme

--- init.el
+++ init.el
@@ -8,3 +8,15 @@
 ;; bootstrap call
 ;; real config goes in config.org
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("a4df5d4a4c343b2712a8ed16bc1488807cd71b25e3108e648d4a26b02bc990b3" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
